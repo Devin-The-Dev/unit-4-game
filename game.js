f2bfca411ab859f0fc5d3b11fd062d15697a8060
Devin Udy <devin.udy@gmail.com>
2018-11-17 22:23:09 -0700

    linked crystal buttons to total, set conditions for winning/loosing the game

--- game.js
+++ game.js
@@ -23,42 +23,65 @@ console.log(f);
 */
 
 var objective = Math.floor(Math.random() * 101 + 19); //In order to get a number between 19 and 120, we need find the difference between the two, then add 19. This sets the range.
-var crystalValue1 = Math.floor(Math.random() * 11 + 1); //leaving this global so it can generate a different number for every button
+var crystalValue1 = Math.floor(Math.random() * 11 + 1); // So if I want a number between 1-12, subtract 1 from 12, then take the difference (11) then add 1 --> it's kind of like this (12-1)+1 //leaving this global so it can generate a different number for every button
 var crystalValue2 = Math.floor(Math.random() * 11 + 1);
 var crystalValue3 = Math.floor(Math.random() * 11 + 1);
 var crystalValue4 = Math.floor(Math.random() * 11 + 1);
+var crystalTotal = 0;
+var win = 0;
+var loose = 0;
 
-function crystal1() {
-  //var crystal1 = Math.floor(Math.random() * 11 + 1); // So if I want a number between 1-12, subtract 1 from 12, then take the difference (11) then add 1 --> it's kind of like this (12-1)+1
-  crystalValue1;
-  console.log(crystalValue1);
-}
+console.log(crystalValue1);
+console.log(crystalValue2);
+console.log(crystalValue3);
+console.log(crystalValue4);
 
-function crystal2() {
-  crystalValue2;
-  console.log(crystalValue2);
-}
-
-function crystal3() {
-  crystalValue3;
-  console.log(crystalValue3);
-}
-
-function crystal4() {
-  crystalValue4;
-  console.log(crystalValue4);
-}
 //Now the buttons need to add up to the total
-
-/*var crystalTotal = function crystalTotal() {
-  crystal1() + crystal2() + crystal3() + crystal4();
-  console.log(crystalTotal());
-};
-
-$(".tNumber").html(crystalTotal);
-*/
 
 $(".oNumber").html(objective);
 
+$("#crystal1").on("click", function crystal1() {
+  crystalTotal = crystalTotal + crystalValue1;
+  console.log("New total= " + crystalTotal);
+  $(".tNumber").text(crystalTotal);
+  gameOver();
+});
+
+$("#crystal2").on("click", function crystal2() {
+  crystalTotal = crystalTotal + crystalValue2;
+  console.log("New total= " + crystalTotal);
+  $(".tNumber").text(crystalTotal);
+  gameOver();
+});
+
+$("#crystal3").on("click", function crystal3() {
+  crystalTotal = crystalTotal + crystalValue3;
+  console.log("New total= " + crystalTotal);
+  $(".tNumber").text(crystalTotal);
+  gameOver();
+});
+
+$("#crystal4").on("click", function crystal4() {
+  crystalTotal = crystalTotal + crystalValue4;
+  console.log("New total= " + crystalTotal);
+  $(".tNumber").text(crystalTotal);
+  gameOver();
+});
+
+//Now I need to determine if the total number wins/loses the game
+// Says I won once page was loaded. Maybe get rid of the function part
+// No, still doesn't know it won
+// I had this in the wrong spot
+// Can I create the function and then call it inside another?
+// Now I want the win/loose to add to their counters
+function gameOver() {
+  if (crystalTotal === objective) {
+    console.log("You win!");
+    win++
+    //Elses can have a conditional statment, but it needs to be else if, and dont include the else statement
+  } else if (crystalTotal > objective) {
+    console.log("You loose!"); //Condition was met!
+    lose++
+  }
+}
 console.log(objective);
-console.log(crystal);
